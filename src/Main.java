dee21470c5a5a361fd80663ef7d74a8e01387a74
ParkBeomMin <club20608@gmail.com>
2019-09-21 17:08:10 +0900

    42586

--- src/Main.java
+++ src/Main.java
@@ -4,10 +4,12 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Solution s = new Solution();
-		int[] a = { 2, 1, 2, 6, 2, 4, 3, 3 };
-		int[] result = s.solution(5, a);
-		for (int i = 0; i < result.length; i++)
-			System.out.print(result[i] + " ");
+		int[] progresses = {93, 30, 55};
+		int[] speeds = {1, 30, 5};
+		int[] answer = s.solution(progresses, speeds);
+		for(int i = 0; i < answer.length; i++) {
+			System.out.print(answer[i] + " ");
+		}
 	}
 
 }
--- src/Solution.java
+++ src/Solution.java
@@ -1,61 +1,33 @@
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+import java.util.LinkedList;
+import java.util.Queue;
 
-public class Solution {
-	public int[] solution(int N, int[] stages) {
-		int[] answer = new int[N];
-		int curUser = stages.length;
-		Arrays.sort(stages);
-		List<Stage> list = new ArrayList<Stage>();
-		for (int i = 0; i < N; i++) {
-			int userCnt = 0;
-			for (int j = 0; j < stages.length; j++) {
-				if (stages[j] == i + 1) {
-					userCnt++;
-				}
-			}
-			if (curUser != 0) {
-				double fail = (double) userCnt / (double) curUser;
-				list.add(new Stage(i + 1, fail));
-			} else {
-				list.add(new Stage(i + 1, 0));
-			}
-
-			curUser -= userCnt;
+class Solution {
+	public int[] solution(int[] progresses, int[] speeds) {
+		int[] answer = {};
+		Queue<Integer> queue = new LinkedList<Integer>();
+		ArrayList<Integer> result = new ArrayList<>();
+		int cnt = 1;
+		for(int i = 0; i < progresses.length; i++) {
+			queue.offer((100-progresses[i])%speeds[i] == 0 ? (100-progresses[i])/speeds[i] : (100-progresses[i])/speeds[i] + 1);
 		}
-		Collections.sort(list);
-		for (int i = 0; i < N; i++) {
-			answer[i] = list.get(i).stageNum;
+		int prev = queue.poll();
+		while(!queue.isEmpty()) {
+			int tmp = queue.poll();
+			if(prev >= tmp) {
+				cnt++;
+			}else {
+				result.add(cnt);
+				cnt = 1;
+				prev = tmp;
+			}
+		}
+		result.add(cnt);
+		answer = new int[result.size()];
+		for(int i = 0; i < result.size(); i++) {
+			answer[i] = result.get(i);
 		}
 		return answer;
-	}
-
-	class Stage implements Comparable<Stage> {
-		int stageNum;
-		double fail;
-
-		public Stage(int stageNum, double fail) {
-			this.stageNum = stageNum;
-			this.fail = fail;
-		}
-
-		@Override
-		public int compareTo(Stage o) {
-			if (o.fail > this.fail) {
-				return 1;
-			} else if (o.fail == this.fail) {
-				if (o.stageNum > this.stageNum) {
-					return -1;
-				} else {
-					return 1;
-				}
-			} else {
-				return -1;
-			}
-
-		}
 	}
 
 }
