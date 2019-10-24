3b23c5205c9f6068fefd767b2e4a3b721e8a0bb3
ParkBeomMin <club20608@gmail.com>
2019-10-24 17:08:38 +0900

    42839

--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,9 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-//		int[] priorities = { 2, 1, 3, 2 };
-//		int location = 2;
-		int[] priorities = { 1, 1, 9, 1, 1, 1 };
-		int location = 0;
+		String numbers = "011";
 		Solution s = new Solution();
-		System.out.print(s.solution(priorities, location));
+		System.out.print(s.solution(numbers));
 	}
 
 }
--- src/Solution.java
+++ src/Solution.java
@@ -1,36 +1,82 @@
-import java.util.LinkedList;
+import java.util.HashSet;
 
 class Solution {
-	public int solution(int[] priorities, int location) {
-		int answer = 0;
-		LinkedList<Integer> waiting = new LinkedList<>();
-		for (int priority : priorities) {
-			waiting.add(priority);
+	int answer = 0;
+
+	public int solution(String numbers) {
+		int[] numberArr = new int[numbers.length()];
+		HashSet<Integer> set = new HashSet<>();
+		for (int i = 0; i < numbers.length(); i++) {
+			numberArr[i] = Integer.parseInt(numbers.substring(i, i + 1));
 		}
-		while (!waiting.isEmpty()) {
-			boolean isMin = true;
-			for (int i = 1; i < waiting.size(); i++) {
-				if (waiting.get(0) < waiting.get(i)) {
-					waiting.add(waiting.poll());
-					if (location == 0) {
-						location = waiting.size() - 1;
-					} else {
-						location--;
-					}
-					isMin = false;
-					break;
-				}
-			}
-			if (isMin) {
-				waiting.poll();
-				answer++;
-				if (location == 0) {
-					break;
-				} else {
-					location--;
-				}
-			}
+		for (int i = 1; i <= numbers.length(); i++) {
+			permutation(numberArr, 0, i, set);
 		}
+		answer = set.size();
 		return answer;
+	}
+
+	void permutation(int[] numbers, int depth, int k, HashSet<Integer> set) {
+		if (depth == k) {
+			int tmp = 0;
+			for (int i = 0; i < k; i++) {
+				tmp += numbers[i] * Math.pow(10, i);
+			}
+			if (isPrime(tmp)) {
+				set.add(tmp);
+			}
+		} else {
+			for (int i = depth; i < numbers.length; i++) {
+				swap(numbers, i, depth);
+				permutation(numbers, depth + 1, k, set);
+				swap(numbers, i, depth);
+			}
+		}
+	}
+
+	void swap(int[] arr, int x, int y) {
+		int tmp = arr[x];
+		arr[x] = arr[y];
+		arr[y] = tmp;
+	}
+
+	void dfs(int[] numbers, boolean[] visited, int p, int[] result, int n, HashSet<Integer> set) {
+		if (p == n) {
+			int tmp = 0;
+			for (int i = 0; i < result.length; i++) {
+				// System.out.print(result[i]);
+				tmp += result[i] * Math.pow(10, i);
+			}
+			// System.out.print(tmp);
+			set.add(tmp);
+			// if (isPrime(tmp)) {
+			// answer++;
+			// }
+
+			System.out.println();
+		} else {
+			for (int i = 0; i < numbers.length; i++) {
+				if (!visited[i]) {
+					visited[i] = true;
+					result[p] = numbers[i];
+					dfs(numbers, visited, p + 1, result, n, set);
+					visited[i] = false;
+				}
+			}
+
+		}
+	}
+
+	boolean isPrime(int num) {
+		if (num <= 1) {
+			return false;
+		} else {
+			for (int i = 2; i < num; i++) {
+				if (num % i == 0) {
+					return false;
+				}
+			}
+		}
+		return true;
 	}
 }
