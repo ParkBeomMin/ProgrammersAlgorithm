92093a0daf02c8a05de34da2e6b0d9cd6e8e712e
ParkBeomMin <club20608@gmail.com>
2019-10-24 22:22:14 +0900

    42840

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		// int[] numbers = {6, 10, 2};
-		// int[] numbers = {121, 12};
-		int[] numbers = { 0, 0, 0, 0 };
+		int[] numbers = { 1, 2, 3, 4, 5 };
 		Solution s = new Solution();
 		System.out.print(s.solution(numbers));
 	}
--- src/Solution.java
+++ src/Solution.java
@@ -1,21 +1,37 @@
-import java.util.Arrays;
-
 class Solution {
-	public String solution(int[] numbers) {
-		String answer = "";
-		String[] arr = new String[numbers.length];
-		for (int i = 0; i < numbers.length; i++) {
-			arr[i] = String.valueOf(numbers[i]);
+	public int[] solution(int[] answers) {
+		int[] answer = {};
+		int[] a = { 1, 2, 3, 4, 5 };
+		int[] b = { 2, 1, 2, 3, 2, 4, 2, 5 };
+		int[] c = { 3, 3, 1, 1, 2, 2, 4, 4, 5, 5 };
+		int aNum = grading(answers, a);
+		int bNum = grading(answers, b);
+		int cNum = grading(answers, c);
+		if (aNum == bNum && bNum == cNum) {
+			return new int[] { 1, 2, 3 };
+		} else if (aNum == bNum && bNum > cNum) {
+			return new int[] { 1, 2 };
+		} else if (aNum == cNum && cNum > bNum) {
+			return new int[] { 1, 3 };
+		} else if (bNum == cNum && cNum > aNum) {
+			return new int[] { 2, 3 };
+		} else if (aNum > bNum && aNum > cNum) {
+			return new int[] { 1 };
+		} else if (bNum > cNum && bNum > aNum) {
+			return new int[] { 2 };
+		} else if (cNum > bNum && cNum > aNum) {
+			return new int[] { 3 };
 		}
-		Arrays.sort(arr, (o1, o2) -> (o2 + o1).compareTo(o1 + o2));
-		if(arr[0].equals("0")) {
-			answer = "0";
-		}else {
-			for (int i = 0; i < arr.length; i++) {
-				answer += arr[i];
-			}	
-		}
-		
 		return answer;
+	}
+
+	int grading(int[] answers, int[] arr) {
+		int result = 0;
+		for (int i = 0; i < answers.length; i++) {
+			if (arr[i % arr.length] == answers[i]) {
+				result++;
+			}
+		}
+		return result;
 	}
 }
