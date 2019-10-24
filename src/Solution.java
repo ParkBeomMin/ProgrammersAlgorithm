00919eb8148cd30bf8d65162ca0ac49b97cf9d43
ParkBeomMin <club20608@gmail.com>
2019-10-24 15:19:59 +0900

    49993

--- src/Solution.java
+++ src/Solution.java
@@ -1,21 +1,26 @@
-import java.util.Arrays;
-
 class Solution {
-	public int[] solution(int[] array, int[][] commands) {
-		int n = commands.length;
-		int[] answer = new int[n];
-		for (int p = 0; p < n; p++) {
-			int i = commands[p][0]-1;
-			int j = commands[p][1]-1;
-			int k = commands[p][2]-1;
-			int[] tmp = new int[j - i + 1];
-			for (int q = i; q <= j; q++) {
-				tmp[q - i] = array[q];
+	public int solution(String skill, String[] skill_trees) {
+		int answer = 0;
+		for (int i = 0; i < skill_trees.length; i++) {
+			String customSkill = skill_trees[i];
+			String tmp = "";
+			for (int j = 0; j < customSkill.length(); j++) {
+				if (skill.contains(customSkill.substring(j, j + 1))) {
+					tmp += customSkill.substring(j, j + 1);
+				}
 			}
-			Arrays.sort(tmp);
-			System.out.println(tmp[k]);
-			answer[p] = tmp[k];
+			if (check(skill, tmp)) {
+				answer++;
+			}
 		}
 		return answer;
+	}
+
+	boolean check(String skill, String tmp) {
+		if (skill.substring(0, tmp.length()).equals(tmp)) {
+			return true;
+		} else {
+			return false;
+		}
 	}
 }
