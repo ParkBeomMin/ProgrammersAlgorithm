92f58957d06eebf5c16a6741db759cf7b76e8ca0
ParkBeomMin <club20608@gmail.com>
2019-10-25 10:34:01 +0900

    42841

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int[] numbers = { 1, 2, 3, 4, 5 };
+		int[][] numbers = { { 123, 1, 1 }, { 356, 1, 0 }, { 327, 2, 0 }, { 489, 0, 1 } };
 		Solution s = new Solution();
 		System.out.print(s.solution(numbers));
 	}
--- src/Solution.java
+++ src/Solution.java
@@ -1,37 +1,52 @@
 class Solution {
-	public int[] solution(int[] answers) {
-		int[] answer = {};
-		int[] a = { 1, 2, 3, 4, 5 };
-		int[] b = { 2, 1, 2, 3, 2, 4, 2, 5 };
-		int[] c = { 3, 3, 1, 1, 2, 2, 4, 4, 5, 5 };
-		int aNum = grading(answers, a);
-		int bNum = grading(answers, b);
-		int cNum = grading(answers, c);
-		if (aNum == bNum && bNum == cNum) {
-			return new int[] { 1, 2, 3 };
-		} else if (aNum == bNum && bNum > cNum) {
-			return new int[] { 1, 2 };
-		} else if (aNum == cNum && cNum > bNum) {
-			return new int[] { 1, 3 };
-		} else if (bNum == cNum && cNum > aNum) {
-			return new int[] { 2, 3 };
-		} else if (aNum > bNum && aNum > cNum) {
-			return new int[] { 1 };
-		} else if (bNum > cNum && bNum > aNum) {
-			return new int[] { 2 };
-		} else if (cNum > bNum && cNum > aNum) {
-			return new int[] { 3 };
-		}
-		return answer;
-	}
-
-	int grading(int[] answers, int[] arr) {
-		int result = 0;
-		for (int i = 0; i < answers.length; i++) {
-			if (arr[i % arr.length] == answers[i]) {
-				result++;
+	public int solution(int[][] baseball) {
+		int answer = 0;
+		for (int i = 123; i <= 987; i++) {
+			int x = i / 100;
+			int y = (i / 10) % 10;
+			int z = i % 10;
+			if (x == 0 || y == 0 || z == 0) {
+				continue;
+			}
+			if (x == y || y == z || x == z) {
+				continue;
+			}
+			for (int j = 0; j < baseball.length; j++) {
+				int strike = 0;
+				int ball = 0;
+				int shot = baseball[j][0];
+				int sx = shot / 100;
+				int sy = (shot / 10) % 10;
+				int sz = shot % 10;
+				if (x == sx) {
+					strike++;
+				}
+				if (y == sy) {
+					strike++;
+				}
+				if (z == sz) {
+					strike++;
+				}
+				if (strike != baseball[j][1]) {
+					break;
+				}
+				if(x == sy || x == sz) {
+					ball++;
+				}
+				if(y == sx || y == sz) {
+					ball++;
+				}
+				if(z == sx || z == sy) {
+					ball++;
+				}
+				if (ball != baseball[j][2]) {
+					break;
+				}
+				if (j == baseball.length - 1) {
+					answer++;
+				}
 			}
 		}
-		return result;
+		return answer;
 	}
 }
